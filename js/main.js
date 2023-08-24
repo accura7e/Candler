17200433b3802c7eb3b5ea5bc303ab724eb109da
accura7e <accura7ee@gmail.com>
2023-08-24 15:49:44 +0300

    add: added burger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$(function(){
+const menuBtn = document.querySelector('.menu__btn')
+const menuMobile = document.querySelector('.header__menu-list')
 
+menuBtn.addEventListener('click', ()=>{
+    menuMobile.classList.toggle('menu--open')
 });
