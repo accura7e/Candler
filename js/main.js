9d87893fe5b187bf75e9bcdcb653f40a1386f977
accura7e <accura7ee@gmail.com>
2023-08-27 13:16:54 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const menuBtn = document.querySelector('.menu__btn')
+const menuMobile = document.querySelector('.header__menu-list')
+
+menuBtn.addEventListener('click', ()=>{
+    menuMobile.classList.toggle('menu--open')
+});
+
+const swiperOne = new Swiper('.feedback__slider', {
+
+    loop: true,
+    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+
+  });
+
+  
+const swiperTwo = new Swiper('.certificates__slider', {
+
+    
+    slidesPerView: '3',
+    spaceBetween: '20',
+  
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+   
+    },
+
+
+  
+
+  });
